b7e06990d33b6e3ed45f4effbcea417effe6b488
Phil Novack-Gottshall <pnovack-gottshall@ben.edu>
2019-07-11 10:07:24 -0500

    clean up spacing and T/TRUE so follows proper style

--- FormatPBDB_Cluster.R
+++ FormatPBDB_Cluster.R
@@ -11,7 +11,7 @@
 
 setwd("C:/Users/pnovack-gottshall/Desktop/Databases/Maintenance & update R scripts")
 
-rm(list=ls())
+rm(list = ls())
 ## Easier if paste link into URL and save manually 
 # pbdb <- read.csv("www.paleobiodb.org/data1.2/taxa/list.csv?base_name=Metazoa&show=app&vocab=pbdb")
 # If want forams too, use base_name=Metazoa,Retaria
@@ -29,13 +29,13 @@ head(pbdb)
 prep.PBDB <- function(g = 1, gen.names, pbdb) {
   scales <- c("phylum", "subphylum", "class", "subclass", "order", "suborder", 
               "superfamily", "family", "subfamily", "genus", "subgenus")
-  out <- data.frame(Phylum=character(1), Subphylum=character(1), 
-                  Class=character(1), Subclass=character(1), 
-                  Order=character(1), Suborder=character(1),
-                  Superfamily=character(1), Family=character(1), 
-                  Subfamily=character(1), Genus=character(1), 
-                  Subgenus=character(1), Species="sp.", 
-                  stringsAsFactors=FALSE)
+  out <- data.frame(Phylum = character(1), Subphylum = character(1), 
+                  Class = character(1), Subclass = character(1), 
+                  Order = character(1), Suborder = character(1),
+                  Superfamily = character(1), Family = character(1), 
+                  Subfamily = character(1), Genus = character(1), 
+                  Subgenus = character(1), Species = "sp.", 
+                  stringsAsFactors = FALSE)
   out$Genus <- as.character(gen.names[g])
   wh <- which(pbdb$accepted_name == out$Genus & (pbdb$taxon_rank == "genus" | 
                                                    pbdb$taxon_rank == "subgenus"))[1]
@@ -73,14 +73,16 @@ unpack.PBDB <- function(prep) {
 # Serial version (running through 1000 genera)
 (start.t <- Sys.time())
 # Use genera and subgenera
-which.gsg <- which(pbdb$accepted_rank=="genus" | pbdb$accepted_rank=="subgenus")
+which.gsg <- which(pbdb$accepted_rank == "genus" | pbdb$accepted_rank == "subgenus")
 n.gen <- length(unique(pbdb$accepted_name[which.gsg]))
-x <- data.frame(Phylum=character(n.gen), Subphylum=character(n.gen), 
-                Class=character(n.gen), Subclass=character(n.gen), Order=character(n.gen), 
-                Suborder=character(n.gen), Superfamily=character(n.gen), Family=character(n.gen), 
-                Subfamily=character(n.gen), Genus=sort(unique(pbdb$accepted_name[which.gsg])), 
-                Subgenus=character(n.gen), Species=rep("sp.", n.gen), early_age=numeric(n.gen),
-                late_age=numeric(n.gen), stringsAsFactors=FALSE)
+x <- data.frame(Phylum = character(n.gen), Subphylum = character(n.gen), 
+                Class = character(n.gen), Subclass = character(n.gen), 
+                Order = character(n.gen), Suborder = character(n.gen), 
+                Superfamily = character(n.gen), Family = character(n.gen), 
+                Subfamily = character(n.gen), Genus = sort(unique(pbdb$accepted_name[which.gsg])), 
+                Subgenus = character(n.gen), Species = rep("sp.", n.gen), 
+                early_age = numeric(n.gen), late_age = numeric(n.gen), 
+                stringsAsFactors = FALSE)
 scales <- c("phylum", "subphylum", "class", "subclass", "order", "suborder", 
             "superfamily", "family", "subfamily", "genus", "subgenus")
 for(g in 1:1000) {
@@ -88,7 +90,8 @@ for(g in 1:1000) {
                                                     pbdb$taxon_rank == "subgenus"))[1]
   x$early_age[g] <- pbdb$firstapp_max_ma[wh]
   x$late_age[g] <- pbdb$lastapp_min_ma[wh]
-  if(pbdb$accepted_rank[wh] == "subgenus") x$Subgenus[g] <- as.character(x$Genus[g])
+  if (pbdb$accepted_rank[wh] == "subgenus")
+    x$Subgenus[g] <- as.character(x$Genus[g])
   parent <- pbdb[which(pbdb$accepted_no == pbdb$parent_no[wh]), ][1, ]
   repeat {
     if(parent$accepted_rank %in% scales) 
@@ -105,7 +108,8 @@ for(g in 1:1000) {
 
 # Re-do using lapply (only 1 CPU)
 (start.t <- Sys.time())
-which.gsg <- which(pbdb$accepted_rank=="genus" | pbdb$accepted_rank=="subgenus")
+which.gsg <-
+  which(pbdb$accepted_rank == "genus" | pbdb$accepted_rank == "subgenus")
 gen.names <- sort(unique(pbdb$accepted_name[which.gsg]))
 # gen.seq <- seq_along(gen.names)
 gen.seq <- 1:1000
@@ -119,7 +123,8 @@ output <- unpack.PBDB(prep)
 library(snowfall)
 (t.start0 <- Sys.time())
 # Initialize
-which.gsg <- which(pbdb$accepted_rank=="genus" | pbdb$accepted_rank=="subgenus")
+which.gsg <-
+  which(pbdb$accepted_rank == "genus" | pbdb$accepted_rank == "subgenus")
 gen.names <- sort(unique(pbdb$accepted_name[which.gsg]))
 gen.seq <- seq_along(gen.names)
 # gen.seq <- 1:1000
@@ -127,9 +132,9 @@ gen.seq <- seq_along(gen.names)
 library(parallel)
 cpus <- parallel::detectCores() # Number of CPUs to cluster together
 # sfSetMaxCPUs(cpus)			      # Use if plan more than 32 CPUs
-sfInit(parallel=T, cpus=cpus, slaveOutfile="initfile") # Initialize cluster
-stopifnot(sfCpus()==cpus)		    # Confirm set up CPUs properly
-stopifnot(sfParallel()==TRUE)		# Confirm now running in parallel
+sfInit(parallel = TRUE, cpus = cpus, slaveOutfile = "initfile") # Initialize cluster
+stopifnot(sfCpus() == cpus)		    # Confirm set up CPUs properly
+stopifnot(sfParallel() == TRUE)		# Confirm now running in parallel
 sfExportAll()				            # Export all libraries, files, & objects
 # Execute the function
 (t.start1 <- Sys.time())
@@ -142,7 +147,7 @@ output2 <- unpack.PBDB(prep)
 (Sys.time() - t.start0)
 (Sys.time() - t.start1)
 head(output2)
-write.csv(output2, file="PBDBformatted.csv", row.names=FALSE)
+write.csv(output2, file = "PBDBformatted.csv", row.names = FALSE)
 
 # Parallel lapply version takes 60.2 secs for 1000 (21 s for the sfLapply) (or 27.8 min for all)
 # Parallel lapply version (w/o export time) takes 19.7 secs for 1000 (or 23.7 mins for all)
@@ -155,7 +160,7 @@ rbind(x[1000, ], output[1000, ], output2[1000, ])
 
 
 
-# x <- read.csv(file="PBDBformatted.csv", header=TRUE, stringsAsFactors=FALSE)
+# x <- read.csv(file = "PBDBformatted.csv", header = TRUE, stringsAsFactors = FALSE)
 head(x)
 
 # Remove terrestrial and non-marine taxa, but include marine tetrapods. List
@@ -217,8 +222,8 @@ table(marine.taxa$Class)
 nrow(x)
 nrow(marine.taxa)
 
-write.csv(marine.taxa, file="PBDBformatted_NoTerr.csv", row.names=FALSE)
-# x <- read.csv(file="PBDBformatted_NoTerr.csv", header=TRUE, stringsAsFactors=FALSE)
+write.csv(marine.taxa, file = "PBDBformatted_NoTerr.csv", row.names = FALSE)
+# x <- read.csv(file = "PBDBformatted_NoTerr.csv", header = TRUE, stringsAsFactors = FALSE)
 
 
 ## Run following to manually combine my database and the PBDB databases
@@ -236,12 +241,13 @@ write.csv(marine.taxa, file="PBDBformatted_NoTerr.csv", row.names=FALSE)
 
 rm(list=ls())
 setwd("C:/Users/pnovack-gottshall/Desktop/Databases/Maintenance & update R scripts")
-# pre <- read.delim(file="PreSizes_Constant_withPBDB.tab", stringsAsFactors=FALSE)
+# pre <- read.delim(file = "PreSizes_Constant_withPBDB.tab", stringsAsFactors = FALSE)
 head(pre)
 tail(pre)
 duplicate.G <- duplicated(pre$Genus)
 post <- pre[!duplicate.G, ]
-write.table(post, file="PreSizes_Constant_withPBDB.tab", quote=FALSE, sep="\t", row.names=FALSE)
+write.table(post, file = "PreSizes_Constant_withPBDB.tab", quote = FALSE, 
+            sep = "\t", row.names = FALSE)
 
 # (3) Copy new entries by Phylum > Subphylum > Class > Subclass > Order.
 
