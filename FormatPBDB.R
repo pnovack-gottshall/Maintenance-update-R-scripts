07c02068de0c5708e73f30bffac6f3f4ab6e10f9
Phil Novack-Gottshall <pnovack-gottshall@ben.edu>
2019-08-29 07:19:36 -0500

    add additional higher taxon ranks so better matches those used by PBDB

--- FormatPBDB.R
+++ FormatPBDB.R
@@ -32,12 +32,14 @@ prep.PBDB <- function(g = 1, gen.names, pbdb) {
   scales <- c("phylum", "subphylum", "class", "subclass", "order", "suborder", 
               "superfamily", "family", "subfamily", "genus", "subgenus")
   out <- data.frame(Phylum = character(1), Subphylum = character(1), 
-                  Class = character(1), Subclass = character(1), 
-                  Order = character(1), Suborder = character(1),
-                  Superfamily = character(1), Family = character(1), 
-                  Subfamily = character(1), Genus = character(1), 
-                  Subgenus = character(1), Species = "sp.", 
-                  stringsAsFactors = FALSE)
+                    Superclass = character(1), Class = character(1), 
+                    Subclass = character(1), Infraclass = character(1), 
+                    Superorder = character(1), Order = character(1), 
+                    Suborder = character(1), Infraorder = character(1), 
+                    Superfamily = character(1), Family = character(1), 
+                    Subfamily = character(1), Genus = character(1), 
+                    Subgenus = character(1), Species = "sp.", 
+                    stringsAsFactors = FALSE)
   out$Genus <- as.character(gen.names[g])
   wh <- which(pbdb$accepted_name == out$Genus & (pbdb$taxon_rank == "genus" | 
                                                    pbdb$taxon_rank == "subgenus"))[1]
