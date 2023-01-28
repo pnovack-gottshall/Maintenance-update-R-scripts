24bd53b4093b7391c7cf3b68751ce64df166bc5f
Phil Novack-Gottshall <pnovack-gottshall@ben.edu>
2023-01-28 06:54:15 -0600

    Code if tallying body size references (plus minor other changes)

--- TallyLHRefs.R
+++ TallyLHRefs.R
@@ -2,8 +2,10 @@
 ## checks any unintended insertion of tabs, double spaces, and other data-entry
 ## errors in the EcologyNotes field.)
 
-# (1) Export ONLY the "IDNumber" (in case of error, allows you to know which entry
-# to correct), "EcologyReference", and "EcologyNotes" fields as "LHRefs.tab".
+# (1) Export ONLY the "IDNumber" (in case of error, allows you to know which
+# entry to correct), "EcologyReference", and "EcologyNotes" fields as
+# "LHRefs.tab". If want to tally body size references, do the same for
+# "BodySizeReference" but do not include "EcologyNotes".
 
 # (2) Open file in MSWord (make sure smart quotes are off: File > Options >
 # Proofing > Autocorrect Options > Autoformat As You Type > uncheck Smart
@@ -30,19 +32,31 @@
 # references, one entry per line/row. Review and edit, if needed. Resave and
 # open here.
 
-# WARNING: THIS PROCESS AND SCRIPT WILL NOT BE ABLE TO DISTINGUISH DIFFERENT
-# REFERENCES WITH THE SAME CITATION FIELDS (e.g., Zamora, et al., 2012a and
-# Zamora, et al., 2012b) UNLESS SO IDENTIFIED IN THE DOWNLOADED DATABASE
-# ENTRIES, SO PRODUCES ONLY A CONSERVATIVE ESTIMATE OF THE NUMBER OF REFERENCES.
-# YOU WILL NEED TO COMPARE THE LIST TO YOUR BIBLIOGRAPHY TO IDENTIFY SUCH
-# INVISIBLE REFERENCES.
+# WARNING: THIS SCRIPT WILL NOT BE ABLE TO DISTINGUISH DIFFERENT REFERENCES WITH
+# THE SAME CITATION FIELDS (e.g., Zamora, et al., 2012a and Zamora, et al.,
+# 2012b) UNLESS THEY ARE SO IDENTIFIED IN THE DOWNLOADED DATABASE ENTRIES, SO
+# PRODUCES ONLY A CONSERVATIVE ESTIMATE OF THE NUMBER OF REFERENCES. YOU WILL
+# NEED TO COMPARE THE LIST TO YOUR BIBLIOGRAPHY TO IDENTIFY SUCH INVISIBLE
+# REFERENCES.
 
 rm(list = ls())
-setwd("C:/Users/pnovack-gottshall/Desktop/Databases/Maintenance & update R scripts")
+setwd("C:/Users/pnovack-gottshall/OneDrive - Benedictine University/Desktop/Databases/Maintenance & update R scripts")
 # setwd("C:/Users/pnovack-gottshall/Documents/GSA (& NSF & NAPC)/2016GSA/GSA2016 analyses")
 input <- read.delim(file = "LHRefs.tab", header = TRUE, colClasses = "character")
+# input <- read.delim(file = "SizeRefs.tab", header = TRUE, colClasses = "character")
 head(input)
 str(input)
+
+# For SizeRefs, need to grep to remove text after the year
+if(ncol(input) == 2L) {
+  for (r in 1:nrow(input)) {
+    input[r, 2] <- strsplit(input[r, 2], ":")[[1]][1]
+  }
+  # To make following code easier (albeit less intuitive)
+  colnames(input)[2] <- "EcologyReference"
+  head(input)
+}
+
 
 # How many unique references?
 length(unique(input$EcologyReference))
